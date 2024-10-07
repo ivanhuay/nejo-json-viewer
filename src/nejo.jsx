d3ae2259c852bc8776bc1a42a732594e4a31e9a5
ivanhuay <ivanhuay@gmail.com>
2024-10-07 14:06:46 -0300

    fix ending comma for arrays

--- src/nejo.jsx
+++ src/nejo.jsx
@@ -2,48 +2,90 @@ import React from 'react';
 
 import styles from './nejo.module.scss';
 
-const Nejo = ({ jsonData, className }) => {
+const Nejo = ({ jsonData, className, endingChar = '' }) => {
     const isArray = Array.isArray(jsonData);
 
-    const TypeRender = ({ value }) => {
+    const TypeRender = ({ value, endingChar }) => {
         if (typeof value === 'string') {
             if (/^(http|https)/.test(value)) {
                 return (
-                    <a href={value} className={styles.link}>
-                        {value}
-                    </a>
+                    <>
+                        <a href={value} className={styles.link}>
+                            {value}
+                        </a>
+                        {endingChar}
+                    </>
                 );
             }
             if (/\S+@\S+\.\S+/.test(value)) {
                 return (
-                    <a href={`mailto:${value}`} className={styles.link}>
-                        {value}
-                    </a>
+                    <>
+                        <a href={`mailto:${value}`} className={styles.link}>
+                            {value}
+                        </a>
+                        {endingChar}
+                    </>
                 );
             }
-            return <div className={styles.text}>{value}</div>;
+            return (
+                <>
+                    <div className={styles.text}>{value}</div>
+                    {endingChar}
+                </>
+            );
         }
         if (typeof value === 'number') {
-            return <div className={styles.number}>{value}</div>;
+            return (
+                <>
+                    <div className={styles.number}>{value}</div>
+                    {endingChar}
+                </>
+            );
         }
         if (typeof value === 'boolean') {
-            return <div className={styles.number}>{value.toString()}</div>;
+            return (
+                <>
+                    <div className={styles.number}>{value.toString()}</div>
+                    {endingChar}
+                </>
+            );
         }
         if (value instanceof Date && !isNaN(value.valueOf())) {
-            return <div className={styles.date}>{value.toISOString()}</div>;
+            return (
+                <>
+                    <div className={styles.date}>{value.toISOString()}</div>
+                    {endingChar}
+                </>
+            );
         }
         if (typeof value === 'object') {
-            return <Nejo jsonData={value} />;
+            return <Nejo jsonData={value} endingChar={endingChar} />;
         }
-        return <div className={styles.defaultType}>{value}</div>;
+        return (
+            <>
+                <div className={styles.defaultType}>{value}</div>
+                {endingChar}
+            </>
+        );
     };
+
     if (isArray) {
         return (
             <div className={styles.container}>
                 <div className={styles.marker}>[</div>
                 {jsonData.map((elmData, i) => (
                     <div key={i} className={styles.arrayElm}>
-                        <Nejo jsonData={elmData} /> {i < jsonData.length - 1 ? ',' : ''}
+                        {Array.isArray(elmData) || typeof elmData === 'object' ? (
+                            <Nejo
+                                jsonData={elmData}
+                                endingChar={i < jsonData.length - 1 ? ',' : ''}
+                            />
+                        ) : (
+                            <TypeRender
+                                value={elmData}
+                                endingChar={i < jsonData.length - 1 ? ',' : ''}
+                            />
+                        )}
                     </div>
                 ))}
                 <div className={styles.marker}>]</div>
@@ -64,13 +106,15 @@ const Nejo = ({ jsonData, className }) => {
                         <div className={styles.key}>{elmKey}</div>
                         <div className={styles.separator}>:</div>
                         <div className={styles.value}>
-                            <TypeRender value={jsonData[elmKey]} />
-                            {i < keys.length - 1 ? ',' : ''}
+                            <TypeRender
+                                value={jsonData[elmKey]}
+                                endingChar={i < keys.length - 1 ? ',' : ''}
+                            />
                         </div>
                     </div>
                 ))}
             </div>
-            <div className={styles.marker}>&#125;</div>
+            <div className={styles.marker}>&#125; {` ${endingChar}`}</div>
         </div>
     );
 };
